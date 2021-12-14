46f8600edf3c22ae8ec58dd1ca18b779729f9de1
hanifiamdev <hanifamrullahdev@gmail.com>
2021-12-14 17:11:53 +0700

    Inisialisasi Project Belajar Java Date & Time

---
+++ src/test/java/com/hanifiamdev/datetime/BelajarJavaDateTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanifiamdev.datetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaDateTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
