084df2209009321894f17c6a791b8fce7ad0fd80
hanifiamdev <hanifamrullahdev@gmail.com>
2021-12-14 17:18:38 +0700

    Date

---
+++ src/test/java/com/hanifiamdev/datetime/DateTest.java
@@ -0,0 +1,20 @@
+package com.hanifiamdev.datetime;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class DateTest {
+
+    @Test
+    void create() {
+
+        var date1 = new Date();
+        var date2 = new Date(System.currentTimeMillis());
+        var date3 = new Date(1615963500000L);
+
+        System.out.println(date1);
+        System.out.println(date2);
+        System.out.println(date3);
+    }
+}
