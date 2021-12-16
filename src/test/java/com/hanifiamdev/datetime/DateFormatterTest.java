c2cb701b4cabea11a984fcc6e9d4f90447478fa9
hanifiamdev <hanifamrullahdev@gmail.com>
2021-12-16 08:09:13 +0700

    Parsing dan Formatting

---
+++ src/test/java/com/hanifiamdev/datetime/DateFormatterTest.java
@@ -0,0 +1,54 @@
+package com.hanifiamdev.datetime;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateFormatterTest {
+
+    @Test
+    void parsing() {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM dd");
+
+        LocalDate localDate = LocalDate.parse("2020 10 25", formatter);
+        System.out.println(localDate);
+
+    }
+
+    @Test
+    void format() {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM dd");
+
+        LocalDate localDate = LocalDate.parse("2020 10 25", formatter);
+        String format = localDate.format(formatter);
+        System.out.println(format);
+
+    }
+
+    @Test
+    void defaultFormatter() {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM dd");
+
+        LocalDate localDate = LocalDate.parse("2020 10 25", formatter);
+        String format = localDate.format(DateTimeFormatter.BASIC_ISO_DATE);
+        System.out.println(format);
+
+    }
+
+    @Test
+    void i18n() {
+
+        Locale locale = new Locale("in", "ID");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MMMM dd", locale);
+
+        LocalDate localDate = LocalDate.of(2020, 1, 1);
+        String format = localDate.format(formatter);
+
+        System.out.println(format);
+    }
+}
